6303288f8efa70a8fdb59659331b45eda044e831
abhishek <abhishekritu810219@gmail.com>
2022-10-16 02:19:06 +0530

    login done

--- routes/index.js
+++ routes/index.js
@@ -2,13 +2,14 @@ const express = require('express');
 
 const router = express.Router();
 
-import {registerController} from '../controllers'
+import {registerController ,loginController} from '../controllers'
 
 // router.get('/', (req, res) => {
 //     res.send('Hello World!')
 //   })
 
 router.post('/register', registerController.register)
+router.post('/login', loginController.login)
 
 
 
